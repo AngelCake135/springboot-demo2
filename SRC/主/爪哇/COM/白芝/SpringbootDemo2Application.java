c293440da0bf66fd3044ad6524445f79e38550d8
AngelCake135 <45910870+AngelCake135@users.noreply.github.com>
2018-12-16 17:18:27 +0800

    Update and rename src/main/java/com/baizhi/SpringbootDemo2Application.java to SRC/主/爪哇/COM/白芝/SpringbootDemo2Application.java

--- SRC/主/爪哇/COM/白芝/SpringbootDemo2Application.java
+++ SRC/主/爪哇/COM/白芝/SpringbootDemo2Application.java
@@ -7,6 +7,20 @@ import tk.mybatis.spring.annotation.MapperScan;
 @MapperScan("com.baizhi.mapper")
 @SpringBootApplication
 public class SpringbootDemo2Application {
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemo2Application.class, args);
